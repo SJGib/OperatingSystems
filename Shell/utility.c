98e8a8a3cb08f4696cc2bf3656f5d86dea4723bc
Stewart <stewart.gibson@uoit.net>
2015-10-08 16:36:06 -0400

    command functions added

--- Shell/utility.c
+++ Shell/utility.c
@@ -15,3 +15,45 @@
 
 // Define your utility functions here, these will most likely be functions that you call
 // in your myshell.c source file
+
+void cmd_prompt(void){
+	
+}
+
+void print(char *content){
+	
+
+	free(content);
+}
+
+void cmd_cd(){
+	
+}
+
+void cmd_clr(void){
+	
+}
+
+char* cmd_dir(){
+	
+}
+
+char* cmd_environ(void){
+	
+}
+
+char* cmd_echo(){
+	
+}
+
+char* cmd_help(void){
+	
+}
+
+void cmd_pause(void){
+	
+}
+
+void cmd_quit(void){
+	
+}
--- Shell/utility.h
+++ Shell/utility.h
@@ -12,4 +12,43 @@
 // extern keyword such as in the following example:
 // extern void display_help(void);
 
+ // prompt input
+extern void cmd_prompt(void);
+
+// print results of commands
+// in: content to be printed
+extern void print(char *content);
+
+// change directory
+// in: new directory
+extern void cmd_cd();
+
+// clear shell
+// no input/output
+extern void cmd_clr(void);
+
+// list directory contents
+// in: directory to be listed
+// out: contents of directory
+extern char* cmd_dir();
+
+// list environment settings
+// out: environment settings
+extern char* cmd_environ(void);
+
+// display comment
+// in: comment to be displayed
+// out: comment to be displayed
+extern char* cmd_echo();
+
+// display user manual
+// out: user manual
+extern char* cmd_help(void);
+
+// pause shell until "enter" is pressed
+extern void cmd_pause(void);
+
+// exit shell
+extern void cmd_quit(void);
+
 #endif /* UTILITY_H_ */
