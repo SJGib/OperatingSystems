d9b80572eaf231fc93babc1c54486df5d6e04980
akiraaida <akira.aida@live.com>
2015-10-22 08:37:40 -0400

    Commented, refactored and removed a warning that was in my section of the code.

--- lab2/utility.h
+++ lab2/utility.h
@@ -1,7 +1,7 @@
 /*
  * MyShell Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2015, <GROUP MEMBERS>
+ * Copyright (C) 2015, <Akira Aida - 100526064, Dennis Pacewicz - 100524231, Truyen Truong - 100516976, S Jack Gibson - 1005145564>
  * All rights reserved.
  * 
  */
